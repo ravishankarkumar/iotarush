58fa6ee198aa86d10ce694298a30d6686151a128
Ravi Shankar <ravishankarkumar@live.com>
2016-10-02 14:37:54 +0530

    started to set up project

---
+++ iotaRush/app/containers/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React, { Component } from 'react'
+
+import {App} from './app'
+
+export default class AppContainer extends Component {
+  render() {
+    return (
+        <App />
+    );
+  }
+}
